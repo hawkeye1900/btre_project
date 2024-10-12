5b1ea9903a574f8c608451fd2981c6f15e67eb9d
hawkeye <173827852+hawkeye1900@users.noreply.github.com>
2024-10-12 06:23:30 +0100

    numerous code updates but specifcally to listings.html whereby bug caused by formatting of django template tags was causing crash. Prettier fomatter disabled and 'black' installed, formatting tags re-written

--- btre/settings.py
+++ btre/settings.py
@@ -32,12 +32,15 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
 	'pages.apps.PagesConfig',
+	'listings.apps.ListingsConfig',
+	'realtors.apps.RealtorsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +79,11 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btredb',
+		'USER': 'postgres',
+		'PASSWORD': 'password',
+		'HOST': 'localhost'	
     }
 }
 
@@ -121,6 +127,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
 	os.path.join(BASE_DIR / 'btre/static'),
 ]
+
+#Media Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
