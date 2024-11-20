17f2e8c6953e3fc533f69d45ea8f3693b25a3827
hawkeye <173827852+hawkeye1900@users.noreply.github.com>
2024-11-20 16:16:39 +0000

    Finished but untested

--- btre/settings.py
+++ btre/settings.py
@@ -34,6 +34,8 @@ INSTALLED_APPS = [
 	'pages.apps.PagesConfig',
 	'listings.apps.ListingsConfig',
 	'realtors.apps.RealtorsConfig',
+	'accounts.apps.AccountsConfig',
+	'contacts.apps.ContactsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -136,3 +138,19 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Messages
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+}
+
+#Email config
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER="i.bowen3894@gmail.com"
+EMAIL_HOST_PASSWORD=""
+EMAIL_USE_TLS=True
